fed022b5b7fe760d89fefd2220f633c682fc3a6d
gom <nobrain908@gmail.com>
2021-10-27 19:55:17 +0900

    #3.4

---
+++ src/Components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import GlobalStyles from "./GlobalStyles";
+import Router from "./Router";
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Router />
+        <GlobalStyles />
+      </>
+    );  
+  }
+}
+
+export default App;
